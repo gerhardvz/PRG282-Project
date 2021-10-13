daad38451b6c60f070eead05e940ae01e4185ae6
Gerhardvz <578244@student.belgiumcampus.ac.za>
2021-10-13 09:00:24 +0200

    Add FileHandler

---
+++ PRG282-Group-Project/Data/FileHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRG282_Group_Project.Business_Layer;
+
+namespace PRG282_Group_Project.Data
+{
+   public class FileHandler
+    {
+
+        string filename = "Users";
+        FileHandler()
+        {
+
+        }
+
+        public User getUser(string username)
+        {
+
+
+        }
+
+
+    }
+}
